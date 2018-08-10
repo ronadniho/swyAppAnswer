4a1f869f890298b5b3240d4800e0ee3b5ad03004
ronadniho <463329429@qq.com>
2018-08-10 18:24:17 +0800

    answerApi

--- src/components/pages/Answer.vue
+++ src/components/pages/Answer.vue
@@ -212,6 +212,7 @@
         studentid: '',
         userid: '',
         sessionId: '',
+        couserid:'',
         examlist: [],//题目集合
         examName: '',//标题
         exampass: 0,//通过分数线
@@ -231,6 +232,7 @@
       this.clo = (this.$route.query.clo != undefined) ? this.$route.query.clo : true;
       this.userid = this.$route.query.userid ? this.$route.query.userid : '';
       this.studentid = this.$route.query.studentid ? this.$route.query.studentid : '';
+      this.couserid = this.$route.query.couserid ? this.$route.query.couserid : '';
       this.state = this.$route.query.state ? this.$route.query.state : false;
       this.android = browser.versions.android;
       this.ios = browser.versions.ios;
@@ -246,15 +248,25 @@
       }
       console.log(this.classid)
       console.log(this.studentid)
+      console.log(this.couserid)
 
       req.request(
-        {
-          key: ["getTopicClassInfo"],
-          postData: {
-            "classid": this.classid,
+        { 
+          "key":["getclasscourseexamlist"],
+            "postData":{
+            "classid":this.classid,
+            "couserid":this.couserid,
             "studentid": this.studentid
           }
         },
+
+//         {
+//           key: ["getTopicClassInfo"],
+//           postData: {
+//             "classid": this.classid,
+//             "studentid": this.studentid
+//           }
+//         },
         '/api/commonhandle',
         true,
         (data) => {
@@ -536,349 +548,345 @@
 </script>
 
 <style scoped lang="less">
-  @import '../../variable';
+@import "../../variable";
 
-  #jq {
-    display: none;
-  }
+#jq {
+  display: none;
+}
 
-  .container {
-    width: @max;
-    height: @max;
-    /*position: absolute;*/
-    /*top: 128px;*/
-    padding-top: 88px;
+.container {
+  width: @max;
+  height: @max;
+  /*position: absolute;*/
+  /*top: 128px;*/
+  padding-top: 88px;
+  box-sizing: border-box;
+}
+
+.answer {
+  padding-bottom: 152px;
+  .action {
+    padding: 0 24px;
     box-sizing: border-box;
-  }
-
-  .answer {
-    padding-bottom: 152px;
-    .action {
-      padding: 0 24px;
-      box-sizing: border-box;
-      border-bottom: 1px solid @answer-col-c9; /*no*/
-      .tip {
-        flex-grow: 1;
-        .base(@answer-col-62, @answer-col-62);
-        line-height: 88px;
+    border-bottom: 1px solid @answer-col-c9; /*no*/
+    .tip {
+      flex-grow: 1;
+      .base(@answer-col-62, @answer-col-62);
+      line-height: 88px;
+      span {
+        .base(@theme, @answer-font-32);
+      }
+      span + span {
+        .base(@answer-col-62, @answer-font-24);
+      }
+    }
+    .action-btn {
+      flex-grow: 1;
+      justify-content: flex-end;
+      a {
+        width: 138px;
+        height: 58px;
+        border: 1px solid @answer-col-c9; /*no*/
+        border-radius: 3px; /*no*/
+        padding-left: 22px;
+        margin-left: 10px;
+        img {
+          width: 26px;
+          height: 33px;
+          vertical-align: middle;
+        }
         span {
-          .base(@theme, @answer-font-32);
-        }
-        span + span {
-          .base(@answer-col-62, @answer-font-24);
-        }
-      }
-      .action-btn {
-        flex-grow: 1;
-        justify-content: flex-end;
-        a {
-          width: 138px;
-          height: 58px;
-          border: 1px solid @answer-col-c9; /*no*/
-          border-radius: 3px; /*no*/
-          padding-left: 22px;
-          margin-left: 10px;
-          img {
-            width: 26px;
-            height: 33px;
-            vertical-align: middle;
-          }
-          span {
-            padding-left: 10px;
-            vertical-align: middle;
-            line-height: 58px;
-            .base(@answer-col-62, @answer-font-20);
-          }
+          padding-left: 10px;
+          vertical-align: middle;
+          line-height: 58px;
+          .base(@answer-col-62, @answer-font-20);
         }
       }
-    }
-
-    .content {
-      padding: 0 24px;
-      box-sizing: border-box;
-      .subject {
-        b {
-          display: block;
-          .base(@answer-col-62, @answer-font-32);
-          line-height: 88px;
-          width: 720px;
-          overflow: hidden;
-          text-overflow: ellipsis;
-          white-space: nowrap;
-        }
-      }
-      .result {
-        padding-top: 46px;
-        box-sizing: border-box;
-        .title {
-          .base(@answer-col-62, @answer-font-28);
-          line-height: 56px;
-        }
-        .single {
-          .box {
-            width: 100%;
-            min-height: 69px;
-            padding: 15px 0;
-          }
-
-          input {
-            display: none;
-          }
-
-          label {
-            width: 100%;
-            height: 100%;
-            display: inline-block;
-            position: relative;
-            line-height: 69px;
-            .base(@answer-col-62, @answer-font-28);
-            padding-left: 100px;
-            box-sizing: border-box;
-          }
-          label:active {
-            background: #EEEEEE;
-          }
-          label:after {
-            content: ""; /*必须设置*/
-            display: inline-block;
-            width: 70px;
-            height: 70px;
-            border: 1px solid @answer-col-c9; /*no*/
-            position: absolute;
-            top: 0px;
-            left: 15px;
-            border-radius: 50%;
-            box-sizing: border-box;
-          }
-
-          input:checked + label:after {
-            /*background-color: @theme;*/
-            background: url(../../assets/raido.png) no-repeat;
-            background-size: 100%;
-          }
-        }
-        .multi {
-          .box {
-            width: 100%;
-            min-height: 75px;
-            padding: 15px 0;
-          }
-          input {
-            display: none;
-          }
-          label {
-            width: 100%;
-            height: 100%;
-            display: inline-block;
-            line-height: 75px;
-            position: relative;
-            .base(@answer-col-62, @answer-font-28);
-            padding-left: 100px;
-            box-sizing: border-box;
-          }
-          label:active {
-            background: #EEEEEE;
-          }
-          label:after {
-            content: "";
-            width: 71px;
-            height: 71px;
-            /*border: 1px solid @answer-col-c9; !*no*!*/
-            display: inline-block;
-            position: absolute;
-            top: 0;
-            left: 15px;
-            background: url(../../assets/no-multi.png) no-repeat;
-            background-size: 100%;
-          }
-          input:checked + label:after {
-            content: "";
-            /* color: @white;
-             text-align: center;
-             font-size: 60px;*/
-            background: url(../../assets/multi.png) no-repeat;
-            background-size: 100%;
-          }
-
-        }
-      }
-    }
-
-    .submit {
-      position: fixed;
-      bottom: 0;
-      background-color: @white;
-      width: @max;
-      height: 152px;
-      padding-top: 32px;
-      box-sizing: border-box;
-      text-align: center;
-      z-index: 100;
-      div {
-        flex-grow: 1;
-        button {
-          background-color: @theme;
-          border-radius: 5px; /*no*/
-          border: 0;
-          width: 258px;
-          line-height: 88px;
-          .base(@white, @answer-font-30);
-        }
-        button.no {
-          color: @answer-col-62;
-          background-color: @white;
-          border: 1px solid @answer-col-c9; /*no*/
-        }
-      }
-
     }
   }
 
-  .answerCard {
-    padding-bottom: 152px;
+  .content {
+    padding: 0 24px;
+    box-sizing: border-box;
     .subject {
-      padding: 0 27px;
-      box-sizing: border-box;
-      border-bottom: 1px solid @answer-col-c9; /*no*/
       b {
+        display: block;
         .base(@answer-col-62, @answer-font-32);
         line-height: 88px;
-        width: 728px;
+        width: 720px;
         overflow: hidden;
         text-overflow: ellipsis;
         white-space: nowrap;
       }
-      a {
-        display: inline-block;
-        font-size: 50px;
-        height: 50px;
-        line-height: 88px;
-      }
     }
-
-    .guide {
-      padding-top: 36px;
-      dl {
-        width: @max;
-        dt {
-          .base(@answer-col-62, @answer-font-28);
-          padding: 0 24px;
-          box-sizing: border-box;
-
-        }
-        dd {
-          .base(@answer-col-62, @answer-font-28);
-          width: 20%;
-          height: 108px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          a {
-            display: block;
-            border-radius: 50%;
-            border: 4px solid @answer-col-c9; /*no*/
-            .base(@answer-col-62, @answer-font-28);
-            width: 68px;
-            height: 68px;
-            line-height: 68px;
-            text-align: center;
-          }
-          a.sel {
-            background-color: @theme;
-            color: @white;
-          }
-          a.sign {
-            border-color: #1F86FF
-          }
-        }
-      }
-    }
-    .submit {
-      position: fixed;
-      bottom: 0;
-      background-color: @white;
-      width: @max;
-      height: 152px;
-      padding-top: 32px;
+    .result {
+      padding-top: 46px;
       box-sizing: border-box;
-      text-align: center;
+      .title {
+        .base(@answer-col-62, @answer-font-28);
+        line-height: 56px;
+      }
+      .single {
+        .box {
+          width: 100%;
+          min-height: 69px;
+          padding: 15px 0;
+        }
+
+        input {
+          display: none;
+        }
+
+        label {
+          width: 100%;
+          height: 100%;
+          display: inline-block;
+          position: relative;
+          line-height: 69px;
+          .base(@answer-col-62, @answer-font-28);
+          padding-left: 100px;
+          box-sizing: border-box;
+        }
+        label:active {
+          background: #eeeeee;
+        }
+        label:after {
+          content: ""; /*必须设置*/
+          display: inline-block;
+          width: 70px;
+          height: 70px;
+          border: 1px solid @answer-col-c9; /*no*/
+          position: absolute;
+          top: 0px;
+          left: 15px;
+          border-radius: 50%;
+          box-sizing: border-box;
+        }
+
+        input:checked + label:after {
+          /*background-color: @theme;*/
+          background: url(../../assets/raido.png) no-repeat;
+          background-size: 100%;
+        }
+      }
+      .multi {
+        .box {
+          width: 100%;
+          min-height: 75px;
+          padding: 15px 0;
+        }
+        input {
+          display: none;
+        }
+        label {
+          width: 100%;
+          height: 100%;
+          display: inline-block;
+          line-height: 75px;
+          position: relative;
+          .base(@answer-col-62, @answer-font-28);
+          padding-left: 100px;
+          box-sizing: border-box;
+        }
+        label:active {
+          background: #eeeeee;
+        }
+        label:after {
+          content: "";
+          width: 71px;
+          height: 71px;
+          /*border: 1px solid @answer-col-c9; !*no*!*/
+          display: inline-block;
+          position: absolute;
+          top: 0;
+          left: 15px;
+          background: url(../../assets/no-multi.png) no-repeat;
+          background-size: 100%;
+        }
+        input:checked + label:after {
+          content: "";
+          /* color: @white;
+             text-align: center;
+             font-size: 60px;*/
+          background: url(../../assets/multi.png) no-repeat;
+          background-size: 100%;
+        }
+      }
+    }
+  }
+
+  .submit {
+    position: fixed;
+    bottom: 0;
+    background-color: @white;
+    width: @max;
+    height: 152px;
+    padding-top: 32px;
+    box-sizing: border-box;
+    text-align: center;
+    z-index: 100;
+    div {
+      flex-grow: 1;
       button {
         background-color: @theme;
         border-radius: 5px; /*no*/
         border: 0;
-        width: 500px;
+        width: 258px;
         line-height: 88px;
         .base(@white, @answer-font-30);
       }
+      button.no {
+        color: @answer-col-62;
+        background-color: @white;
+        border: 1px solid @answer-col-c9; /*no*/
+      }
+    }
+  }
+}
 
+.answerCard {
+  padding-bottom: 152px;
+  .subject {
+    padding: 0 27px;
+    box-sizing: border-box;
+    border-bottom: 1px solid @answer-col-c9; /*no*/
+    b {
+      .base(@answer-col-62, @answer-font-32);
+      line-height: 88px;
+      width: 728px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+    a {
+      display: inline-block;
+      font-size: 50px;
+      height: 50px;
+      line-height: 88px;
     }
   }
 
-  .modal {
-    position: fixed;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    width: @max;
-    height: @max;
-    background-color: rgba(0, 0, 0, .4);
-    z-index: 1000;
-    img {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      width: 300px;
-      height: 300px;
-      margin-left: -150px;
-      margin-top: -150px;
-    }
-    .modal-diag {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      margin-left: -310px;
-      margin-top: -309px;
-      .modal-bg {
-        width: 620px;
-        height: 520px;
-        background: url(../../assets/modal-bg.png) no-repeat;
-        background-size: cover;
-        position: relative;
-        p {
-          width: 520px;
-          color: @theme;
-          font-size: 30px;
-          position: absolute;
-          bottom: 46px;
-          left: 50%;
-          margin-left: -260px;
+  .guide {
+    padding-top: 36px;
+    dl {
+      width: @max;
+      dt {
+        .base(@answer-col-62, @answer-font-28);
+        padding: 0 24px;
+        box-sizing: border-box;
+      }
+      dd {
+        .base(@answer-col-62, @answer-font-28);
+        width: 20%;
+        height: 108px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        a {
+          display: block;
+          border-radius: 50%;
+          border: 4px solid @answer-col-c9; /*no*/
+          .base(@answer-col-62, @answer-font-28);
+          width: 68px;
+          height: 68px;
+          line-height: 68px;
           text-align: center;
         }
-      }
-      .btn-group {
-        width: @max;
-        height: 98px;
-        display: table;
-        content: '';
-        clear: both;
-        button {
-          color: @white;
-          font-size: 30px;
+        a.sel {
           background-color: @theme;
-          height: 98px;
-          line-height: 98px;
-          border: 0;
-          width: @max/2;
-          box-sizing: border-box;
-          padding: 0;
-          margin: 0;
-          float: left;
+          color: @white;
         }
-        button:last-child {
-          background-color: #918181;
+        a.sign {
+          border-color: #1f86ff;
         }
       }
     }
   }
+  .submit {
+    position: fixed;
+    bottom: 0;
+    background-color: @white;
+    width: @max;
+    height: 152px;
+    padding-top: 32px;
+    box-sizing: border-box;
+    text-align: center;
+    button {
+      background-color: @theme;
+      border-radius: 5px; /*no*/
+      border: 0;
+      width: 500px;
+      line-height: 88px;
+      .base(@white, @answer-font-30);
+    }
+  }
+}
+
+.modal {
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  width: @max;
+  height: @max;
+  background-color: rgba(0, 0, 0, 0.4);
+  z-index: 1000;
+  img {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 300px;
+    height: 300px;
+    margin-left: -150px;
+    margin-top: -150px;
+  }
+  .modal-diag {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-left: -310px;
+    margin-top: -309px;
+    .modal-bg {
+      width: 620px;
+      height: 520px;
+      background: url(../../assets/modal-bg.png) no-repeat;
+      background-size: cover;
+      position: relative;
+      p {
+        width: 520px;
+        color: @theme;
+        font-size: 30px;
+        position: absolute;
+        bottom: 46px;
+        left: 50%;
+        margin-left: -260px;
+        text-align: center;
+      }
+    }
+    .btn-group {
+      width: @max;
+      height: 98px;
+      display: table;
+      content: "";
+      clear: both;
+      button {
+        color: @white;
+        font-size: 30px;
+        background-color: @theme;
+        height: 98px;
+        line-height: 98px;
+        border: 0;
+        width: @max / 2;
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+        float: left;
+      }
+      button:last-child {
+        background-color: #918181;
+      }
+    }
+  }
+}
 </style>
